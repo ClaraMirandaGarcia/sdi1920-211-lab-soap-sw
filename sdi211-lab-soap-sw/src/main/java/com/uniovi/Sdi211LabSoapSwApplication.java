7eecaeb16a28ded7a18ac2475716903f64e05f64
Clara <UO264958@uniovi.es>
2020-04-22 17:52:22 +0200

    SDI-SW-11.1 Servicio SOAP

---
+++ sdi211-lab-soap-sw/src/main/java/com/uniovi/Sdi211LabSoapSwApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi211LabSoapSwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdi211LabSoapSwApplication.class, args);
+	}
+
+}
